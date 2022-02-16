849abe359eda70a514a54f9e9cc037cc10630ac3
diegorguez <98813907+diegorguez@users.noreply.github.com>
2022-02-16 17:41:04 +0100

    Add files via upload

---
+++ decker01.py
@@ -0,0 +1,35 @@
+from multiprocessing import Process
+from multiprocessing import current_process
+from multiprocessing import Value, Array
+N = 8
+def task(common, tid, turn):
+   a = 0
+   for i in range(100):
+      print(f'{tid}â{i}: Nonâcritical Section')
+      a += 1
+      print(f'{tid}â{i}: End of nonâcritical Section')
+      while turn.value!=tid:
+          pass
+      print(f'{tid}â{i}: Critical section')
+      v = common.value + 1
+      print(f'{tid}â{i}: Inside critical section')
+      common.value = v
+      print(f'{tid}â{i}: End of critical section')
+      turn.value = (tid + 1) % N
+
+def main():
+    lp = []
+    common = Value('i', 0)
+    turn = Value('i', 0)
+    for tid in range(N):
+        lp.append(Process(target=task, args=(common, tid, turn)))
+    print (f"Valor inicial del contador {common.value}")
+    for p in lp:
+        p.start()
+    for p in lp:
+        p.join()
+    print (f"Valor final del contador {common.value}")
+    print ("fin")
+
+if __name__ == "__main__":
+    main()
